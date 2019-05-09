e1de4dbf93cb93cf4cb9ccaf980b7dcdd63cec6f
Aleksey Krichevskiy <boolivar@gmail.com>
2019-05-09 21:38:58 +0300

    Noop SpiFactoryImpl method arguments fix

--- src/noop/spifactoryimpl.cpp
+++ src/noop/spifactoryimpl.cpp
@@ -3,10 +3,10 @@
 #include "printchipselect.h"
 #include "printspi.h"
 
-std::unique_ptr<Spi> SpiFactoryImpl::createSpi() {
+std::unique_ptr<Spi> SpiFactoryImpl::createSpi(uint8_t index) {
     return std::make_unique<PrintSpi>();
 }
 
-std::unique_ptr<ChipSelect> SpiFactoryImpl::createChipSelect() {
+std::unique_ptr<ChipSelect> SpiFactoryImpl::createChipSelect(uint8_t pin) {
     return std::make_unique<PrintChipSelect>();
 }
