ed9fe45e30c65265acdf0749195ad73d6a724a3e
Aleksey Krichevskiy <boolivar@gmail.com>
2019-05-13 00:27:50 +0300

    Config class

---
+++ src/config.cpp
@@ -0,0 +1,37 @@
+#include "config.h"
+
+Config::Config(uint8_t cs, uint32_t size, uint32_t page, avr_mem_t memory, file_format_t format, bool verbose, bool write, const std::string &fileName)
+    : cs(cs), size(size), page(page), memory(memory), format(format), verbose(verbose), write(write), fileName(fileName) {
+}
+
+uint8_t Config::getCs() const {
+    return cs;
+}
+
+uint32_t Config::getSize() const {
+    return size;
+}
+
+uint32_t Config::getPage() const {
+    return page;
+}
+
+avr_mem_t Config::getMemory() const {
+    return memory;
+}
+
+file_format_t Config::getFormat() const {
+    return format;
+}
+
+std::string Config::getFileName() const {
+    return fileName;
+}
+
+bool Config::isVerbose() const {
+    return verbose;
+}
+
+bool Config::isWrite() const {
+    return write;
+}
---
+++ src/config.h
@@ -0,0 +1,50 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <cstdint>
+#include <string>
+
+typedef enum {
+    LOCK,
+    FUSE,
+    FLASH,
+    EEPROM
+} avr_mem_t;
+
+typedef enum {
+    BIN,
+    HEX
+} file_format_t;
+
+class Config {
+public:
+    Config(uint8_t cs,
+        uint32_t size,
+        uint32_t page,
+        avr_mem_t memory,
+        file_format_t format,
+        bool verbose,
+        bool write,
+        const std::string& fileName);
+
+    uint8_t getCs() const;
+    uint32_t getSize() const;
+    uint32_t getPage() const;
+    avr_mem_t getMemory() const;
+    file_format_t getFormat() const;
+    bool isVerbose() const;
+    bool isWrite() const;
+    std::string getFileName() const;
+
+private:
+    uint8_t cs;
+    uint32_t size;
+    uint32_t page;
+    avr_mem_t memory;
+    file_format_t format;
+    bool verbose;
+    bool write;
+    std::string fileName;
+};
+
+#endif // CONFIG_H
