096bc805ea9f27cd24e44f1452d08b8ace6991c4
Aleksey Krichevskiy <boolivar@gmail.com>
2019-05-31 01:32:25 +0300

    RawDataReader vector iterator initialization

--- src/rawdatareader.cpp
+++ src/rawdatareader.cpp
@@ -1,10 +1,7 @@
 #include "rawdatareader.h"
 
-#include <algorithm>
 #include <iterator>
 
 std::vector<uint8_t> RawDataReader::readData(std::istream& in) {
-    std::vector<uint8_t> data;
-    std::copy(std::istream_iterator<uint8_t>(in), std::istream_iterator<uint8_t>(), std::back_inserter(data));
-    return data;
+    return std::vector<uint8_t>(std::istream_iterator<uint8_t>(in), std::istream_iterator<uint8_t>());
 }
